3faa13a72f441d0c1103f45a6c27e90c38d07510
su497907 <su497907@ucf.edu>
2025-03-02 22:08:53 -0500

    Create R16_ENV01_J.java
    Rule 16 (Correct)

---
+++ R16_ENV01_J.java
@@ -0,0 +1,15 @@
+package trusted;
+ 
+public class MixMatch {
+  // ...
+}
+ 
+ 
+// In the same signed & sealed JAR file:
+package trusted;
+ 
+class RetValue {
+  int getValue() {
+    return 1;
+  }
+}
