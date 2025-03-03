adbeb0b150e0597394a8054bd9f6a3eaba2562dc
su497907 <su497907@ucf.edu>
2025-03-02 22:07:56 -0500

    Create R16_ENV01_J.java
    Rule 16 (Incorrect)

---
+++ R16_ENV01_J.java
@@ -0,0 +1,41 @@
+package trusted;
+import untrusted.RetValue;
+ 
+public class MixMatch {
+  private void privilegedMethod() throws IOException {
+    try {
+      final FileInputStream fis = AccessController.doPrivileged(
+        new PrivilegedExceptionAction<FileInputStream>() {
+          public FileInputStream run() throws FileNotFoundException {
+            return new FileInputStream("file.txt");
+          }
+        }
+      );
+      try {
+        RetValue rt = new RetValue();
+ 
+        if (rt.getValue() == 1) {
+          // Do something with sensitive file
+        }
+      } finally {
+        fis.close();
+      }
+    } catch (PrivilegedActionException e) {
+      // Forward to handler and log
+    }
+  }
+ 
+  public static void main(String[] args) throws IOException {
+    MixMatch mm = new MixMatch();
+    mm.privilegedMethod();
+  }
+}
+ 
+// In another JAR file:
+package untrusted;
+ 
+class RetValue {
+  public int getValue() {
+    return 1;
+  }
+}
